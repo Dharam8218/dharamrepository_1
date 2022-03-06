17fd15380e3a69ef0b0189c804af059656ae9778
Dharam8218 <dharamveersinghgtb@gmail.com>
2022-03-06 15:22:04 +0530

    written some code in Demo2 and Demo3

---
+++ Demo2.java
@@ -0,0 +1,7 @@
+class Demo2{
+      int a = 100;
+      String name = "Dharam";
+  public static void main(String[] args) {
+
+  }
+}
---
+++ Demo3.java
@@ -0,0 +1,9 @@
+class Demo3{
+
+      void m1(){
+        
+      }
+  public static void main(String[] args) {
+
+  }
+}
